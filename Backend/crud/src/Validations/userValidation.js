d7fc208df7492a8ded8258d8a6e482d373a4ab4c
Madson Luiz <madsonlz369@gmail.com>
2021-10-15 00:51:03 -0300

    validação do cpf e do agent code

--- Backend/crud/src/Validations/userValidation.js
+++ Backend/crud/src/Validations/userValidation.js
@@ -1,12 +1,22 @@
 const yup = require("yup");
+const valBr = require("validations-br");
+const hasLogin = require("../Querry/index");
 
 const userSchema = yup.object({
-    Nome: yup.string().required(),
-    login: yup.string().required(),
-    CPF:yup.string(),
-    Email:yup.string().email().required(),
-    AgentCode:yup.string().min(4).required(),
-    
+  Nome: yup.string().required(),
+  login: yup.string().required(),
+  CPF: yup
+    .string()
+    .test("is cpf", "CPF invalido", (value) => valBr.validateCPF(value)),
+  Email: yup.string().email().required(),
+  AgentCode: yup
+    .string()
+    .test("Digits only", "AgentCode deve conter somente digitos", (value) =>
+      /^\d+$/.test(value)
+    )
+    .min(6)
+    .max(6)
+    .required(),
 });
 
 module.exports = userSchema;

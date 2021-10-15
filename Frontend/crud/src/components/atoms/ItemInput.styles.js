0e33acb5f3f6077dc596fc349ab0f3360ce031e5
Madson Luiz <madsonlz369@gmail.com>
2021-10-15 00:54:08 -0300

    novos components

--- Frontend/crud/src/components/atoms/ItemInput.styles.js
+++ Frontend/crud/src/components/atoms/ItemInput.styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-  width: 150px;
+  width: 200px;
   margin-bottom: 10px;
 `;
---
+++ Frontend/crud/src/components/atoms/TextForm.styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const FormSpan = styled.span`
+  position: absolute;
+`;
